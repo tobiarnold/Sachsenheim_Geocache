a1fdacac1107d3c43d3b741bf797045571ae35c3
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2022-07-27 18:35:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ function generatePDF() {
     var img = document.getElementById('bild');
     doc.addImage(img, 'JPEG', 90, 10);
     doc.setFontSize(14)
-    var str = "Wir sind die Initiative „Sachsenheim wird klimaneutral bis 2035“,\n die den Klimawandel vor Ort mutig angehen und eine\n      nachhaltige Zukunft für uns alle schaffen will.";
+    var str = "Wir sind die Initiative „Sachsenheim wird klimaneutral bis 2035“,\n     die den Klimawandel vor Ort mutig angehen und eine\n       nachhaltige Zukunft für uns alle schaffen will.";
     doc.text(str, 105, 130, null, null, "center"); 
     doc.setFontSize(16)
     doc.text("Du möchtest mehr über uns erfahren?", 100, 160, "center");  
     doc.text("Alle Informationen und Kontaktdaten findest du hier:", 100, 170, "center");   
-    doc.text("https://sachsenheim-klimaneutral.de", 60, 195);
+    doc.text("https://sachsenheim-klimaneutral.de", 57, 190);
     doc.textWithLink('', 155, 195, {url: 'https://sachsenheim-klimaneutral.de'});
     doc.save("Urkunde.pdf");
 }
