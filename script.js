ed224d9f976d1d3db0a34761eb6318c8e82ba6ec
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2022-07-27 18:09:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,16 +5,23 @@ function generatePDF() {
     var today = new Date();
     var date = today.getDate()+'.'+(today.getMonth()+1)+'.'+ today.getFullYear();
     doc.setFontSize(40);
-    doc.text("Urkunde Geocache", 105, 25, null, null, "center");
+    doc.setFillColor(255, 200, 45);
+    doc.rect(0, 0, 250, 500, "F");
+    doc.text("Urkunde Geocache", 105, 65, null, null, "center");
     doc.setFontSize(16);
     var input_user=document.getElementById("input1").value;
-    doc.text("Herzlichen Glückwunsch "+input_user, 105, 55, null, null, "center");
-    doc.text("Du hast den Geocache erfolgreich gemeistert!", 105, 70, null, null, "center");
-    doc.text("Sachsenheim, den "+date, 105, 85, null, null, "center");
+    doc.text("Herzlichen Glückwunsch "+input_user+", ", 105, 85, null, null, "center");
+    doc.text("Du hast den Geocache erfolgreich gemeistert!", 105, 95, null, null, "center");
+    doc.text("Sachsenheim, den "+date, 105, 110, null, null, "center");
     var img = document.getElementById('bild');
-    doc.addImage(img, 'JPEG', 1, 2);
-    doc.text("https://sachsenheim-klimaneutral.de", 105, 100, null, null, "center");
-    doc.textWithLink('', 155, 100, {url: 'https://sachsenheim-klimaneutral.de'});
+    doc.addImage(img, 'JPEG', 90, 10);
+    doc.setFontSize(14)
+    var str = "Wir sind die Initiative „Sachsenheim wird klimaneutral bis 2035“,\n die den Klimawandel vor Ort mutig angehen und eine\n nachhaltige Zukunft für uns alle schaffen will.";
+    doc.text(str, 105, 130, null, null, "center"); 
+    doc.setFontSize(16)
+    doc.text("Du möchtest mehr über uns erfahren?", 100, 160, "center");  
+    doc.text("Alle Informationen und Kontaktdaten findest du hier:", 100, 170, "center");   
+    doc.text("https://sachsenheim-klimaneutral.de", 60, 195);
+    doc.textWithLink('', 155, 195, {url: 'https://sachsenheim-klimaneutral.de'});
     doc.save("Urkunde.pdf");
 }
-           
